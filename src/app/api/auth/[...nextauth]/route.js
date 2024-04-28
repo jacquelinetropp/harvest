a497bd337e218d90839cf3b70f36c9427119afcf
unknown <jacquelinetropp@gmail.com>
2024-04-28 15:45:30 -0500

    troubleshooting

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,53 +1,17 @@
-import clientPromise from "@/libs/mongoConnect";
-import {UserInfo} from "@/models/UserInfo";
-import bcrypt from "bcrypt";
-import * as mongoose from "mongoose";
-import {User} from '@/models/User';
+// import clientPromise from "@/libs/mongoConnect";
+// import {UserInfo} from "@/models/UserInfo";
+// import bcrypt from "bcrypt";
+// import * as mongoose from "mongoose";
+// import {User} from '@/models/User';
 import NextAuth, {getServerSession} from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import { isAdmin } from "../../../authUtils";
-
-export const authOptions = {
-  secret: process.env.SECRET,
-  adapter: MongoDBAdapter(clientPromise),
-  session: {
-    strategy: 'jwt'
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-    CredentialsProvider({
-      name: 'Credentials',
-      id: 'credentials',
-      credentials: {
-        username: { label: "Email", type: "email", placeholder: "test@example.com" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const email = credentials?.email;
-        const password = credentials?.password;
-
-        mongoose.connect(process.env.MONGO_URL);
-        const user = await User.findOne({ email });
-        const passwordOk = user && bcrypt.compareSync(password, user.password);
-
-        if (passwordOk) {
-          return user;
-        }
-
-        return null
-      }
-    })
-  ],
-};
+// import CredentialsProvider from "next-auth/providers/credentials";
+// import GoogleProvider from "next-auth/providers/google";
+// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import { isAdmin, authOptions } from "../../../authUtils";
 
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST }
 
 // Exporting the isAdmin function if needed elsewhere
-export { isAdmin };
+export { isAdmin, authOptions };
