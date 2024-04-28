fd05ee223a106e34dd9e34ab329d89e96d6c0836
unknown <jacquelinetropp@gmail.com>
2024-04-28 15:43:50 -0500

    troubleshooting

--- src/app/authUtils.js
+++ src/app/authUtils.js
@@ -1,15 +1,55 @@
-import { getServerSession } from "next-auth";
-import { UserInfo } from "@/models/UserInfo";
+import clientPromise from "@/libs/mongoConnect";
+import {UserInfo} from "@/models/UserInfo";
+import bcrypt from "bcrypt";
+import * as mongoose from "mongoose";
+import {User} from '@/models/User';
+import {getServerSession} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
-export async function isAdmin(authOptions) {
-  const session = await getServerSession(authOptions);
-  const userEmail = session?.user?.email;
-  if (!userEmail) {
-    return false;
+export const authOptions = {
+    secret: process.env.SECRET,
+    adapter: MongoDBAdapter(clientPromise),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      }),
+      CredentialsProvider({
+        name: 'Credentials',
+        id: 'credentials',
+        credentials: {
+          username: { label: "Email", type: "email", placeholder: "test@example.com" },
+          password: { label: "Password", type: "password" },
+        },
+        async authorize(credentials, req) {
+          const email = credentials?.email;
+          const password = credentials?.password;
+  
+          mongoose.connect(process.env.MONGO_URL);
+          const user = await User.findOne({email});
+          const passwordOk = user && bcrypt.compareSync(password, user.password);
+  
+          if (passwordOk) {
+            return user;
+          }
+  
+          return null
+        }
+      })
+    ],
+  };
+  
+  export async function isAdmin() {
+    const session = await getServerSession(authOptions);
+    const userEmail = session?.user?.email;
+    if (!userEmail) {
+      return false;
+    }
+    const userInfo = await UserInfo.findOne({email:userEmail});
+    if (!userInfo) {
+      return false;
+    }
+    return userInfo.admin;
   }
-  const userInfo = await UserInfo.findOne({ email: userEmail });
-  if (!userInfo) {
-    return false;
-  }
-  return userInfo.admin;
-}
